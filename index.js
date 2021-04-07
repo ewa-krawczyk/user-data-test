69eb37d1594bb555ca7ded63816b812f2245466f
ewa-krawczyk <75725771+ewa-krawczyk@users.noreply.github.com>
2021-04-07 18:05:20 +0200

    #8 Break indentation problem

--- index.js
+++ index.js
@@ -21,19 +21,19 @@ do {
   switch(command) {
     case "add":
       actionAdd();
-    break;
+      break;
     case "list":
       actionList();
-    break;
+      break;
     case "remove":
       actionRemove();
-    break;
+      break;
     case "help":
       actionHelp();
-    break;
+      break;
     default:
       actionInvalid();
-    break;
+      break;
     }
 } while (command != 'quit')
 
